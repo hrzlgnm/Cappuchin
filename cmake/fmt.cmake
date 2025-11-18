44d24ccc13398b15af0faa288cc69e7dcee04bc4
hrzlgnm <hrzlgnm@users.noreply.github.com>
2025-11-18 12:49:38 +0100

    Monka

--- cmake/fmt.cmake
+++ cmake/fmt.cmake
@@ -1,6 +1,6 @@
 include(FetchContent)
 
-FetchContent_Declare(fmt GIT_REPOSITORY https://github.com/fmtlib/fmt.git GIT_TAG 12.1.0)
+FetchContent_Declare(fmt GIT_REPOSITORY "https://github.com/fmtlib/fmt.git" GIT_TAG "12.1.0")
 
 FetchContent_MakeAvailable(fmt)
 
