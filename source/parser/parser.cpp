050a2627bf5d8f55624c2bcba8737dc263c8a25b
hrzlgnm <hrzlgnm@users.noreply.github.com>
2025-11-23 21:32:29 +0100

    monka

--- source/parser/parser.cpp
+++ source/parser/parser.cpp
@@ -1375,7 +1375,8 @@ TEST_CASE("emptyHashLiteral")
     auto [prgrm, _] = check_program(R"({})");
     auto* hash_lit = require_expression<hash_literal>(prgrm);
     REQUIRE(hash_lit->pairs.empty());
-    REQUIRE(hash_lit->loc() == location {"<stdin>", 1, 1});
+    const auto expected_location = location {"<stdin>", 1, 1};
+    REQUIRE(hash_lit->loc() == expected_location);
 }
 
 TEST_CASE("nullLiteral")
