e944553b08dd8c586b3b637a768de6ad6cceeab6
hrzlgnm <hrzlgnm@users.noreply.github.com>
2025-11-23 10:44:50 +0100

    monka monka doubles
    Closes #463

--- source/object/object.cpp
+++ source/object/object.cpp
@@ -51,18 +51,20 @@ auto are_almost_equal(const double a, const double b) -> bool
 template<typename T>
 auto eq_helper(const T* t, const object& other) -> const object*
 {
-    if constexpr (std::is_same_v<std::decay_t<T>, decimal_object>) {
-        return native_bool_to_object(other.is(t->type()) && are_almost_equal(t->value, other.val<decimal_object>()));
+    if constexpr (std::is_floating_point_v<decltype(T::value)>) {
+        return native_bool_to_object(other.is(t->type()) && are_almost_equal(t->value, other.as<T>()->value));
     }
     return native_bool_to_object(other.is(t->type()) && t->value == other.as<T>()->value);
+}
+
+auto value_eq_helper(double lhs, double rhs) -> const object*
+{
+    return native_bool_to_object(are_almost_equal(lhs, rhs));
 }
 
 template<typename T>
 auto value_eq_helper(const T& lhs, const T& rhs) -> const object*
 {
-    if constexpr (std::is_same_v<std::decay_t<T>, double>) {
-        return native_bool_to_object(are_almost_equal(lhs, rhs));
-    }
     return native_bool_to_object(lhs == rhs);
 }
 
