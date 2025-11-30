513821eee8d6a3aecf23746ba0c76280ec204e2e
hrzlgnm <hrzlgnm@users.noreply.github.com>
2025-11-30 19:39:51 +0100

    chore(ci): Externalize clang format check (#518)

--- cmake/dev-mode.cmake
+++ cmake/dev-mode.cmake
@@ -9,7 +9,4 @@ add_subdirectory(test)
 add_custom_target(run-exe COMMENT runs executable COMMAND cappuchin_exe VERBATIM)
 add_dependencies(run-exe cappuchin_exe)
 
-include(cmake/lint-targets.cmake)
-include(cmake/spell-targets.cmake)
-
 add_folders(Project)
